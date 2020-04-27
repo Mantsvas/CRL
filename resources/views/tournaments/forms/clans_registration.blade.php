5c21b72ac2e43612924b0adbb7ec9b6eaea39395
Mantas Gelezinis <Mantsvas@gmail.com>
2020-04-27 20:40:38 +0300

    .

--- resources/views/tournaments/forms/clans_registration.blade.php
+++ resources/views/tournaments/forms/clans_registration.blade.php
@@ -11,10 +11,10 @@
     <x-headings.extra-small :heading="__('messages.Team members') . ' (' . __('messages.Min') . ': 5)'" />
     @for ($i = 1; $i <= $tournament->teamCapacity(); $i++)
         <div class="row">
-            <div class="col-6">
+            <div class="col-12 col-xs-6">
                 <x-inputs.text :label="$i. '. ' . __('messages.Player tag') . ' #'" name="player_tag[]" :required="$i <= 5 ? 'required' : null" />
             </div>
-            <div class="col-6">
+            <div class="col-12 col-xs-6">
                 <x-inputs.text :label="__('messages.Player name')" name="player_name[]" :required="$i <= 5 ? 'required' : null"/>
             </div>
         </div>
