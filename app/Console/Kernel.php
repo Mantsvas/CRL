096d9d0c59048a23461dc0e78f55bf803d8189a5
Mantas Gelezinis <Mantsvas@gmail.com>
2020-11-13 20:26:41 +0200

    .

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->command(UpdateCWResults::class)->everyMinute();
+        $schedule->command(UpdateCWResults::class)->everyTenMinutes();
     }
 
     /**
