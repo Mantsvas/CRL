d6534d450116f234c4b7a440c365541c1ad8eacf
Mantas Gelezinis <Mantsvas@gmail.com>
2020-11-15 03:30:08 +0200

    .

--- app/Console/Commands/UpdateCWResults.php
+++ app/Console/Commands/UpdateCWResults.php
@@ -47,6 +47,7 @@ class UpdateCWResults extends Command
      */
     public function handle()
     {
+        \Log::info('UPDATE CW');
         $clans = Cnst::CLAN_TAGS;
         $riverRaces = CurrentRiverRace::whereIn('clan_tag', $clans)->get();
         foreach ($clans as $tag) {
