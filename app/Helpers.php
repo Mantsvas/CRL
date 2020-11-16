94706a953be19b4167036c4539c0920421a1271b
Mantas Gelezinis <Mantsvas@gmail.com>
2020-11-16 15:04:08 +0200

    .

--- app/Helpers.php
+++ app/Helpers.php
@@ -8,6 +8,7 @@ class Helpers
 {
     public static function convertUTCDate($str)
     {
+        \Log::info($str);
         $date = $str[0] . $str[1] . $str[2] . $str[3] . '-' . $str[4] . $str[5] . '-' . $str[6] . $str[7] . ' ' . $str[9] . $str[10] . ':' . $str[11] . $str[12] . ':' . $str[13] . $str[14];
         $date = Carbon::parse($date);
         $date->addHours(7);
