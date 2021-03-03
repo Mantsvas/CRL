d5ee778131b7cf12fa4759a3c1f8deeb4825cd91
Mantas Gelezinis <Mantsvas@gmail.com>
2021-03-03 23:13:23 +0200

    .

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -33,7 +33,7 @@ class Kernel extends ConsoleKernel
         $schedule->command(UpdateCWResults::class)->everyTenMinutes();
         $schedule->command(UpdateClans::class)->hourly();
         // $schedule->command(UpdatePlayers::class)->weeklyOn(1, '12:00')->timezone('Europe/Vilnius');
-        $schedule->command(UpdateRiverRaceLog::class)->weeklyOn(1, '13:00');
+        $schedule->command(UpdateRiverRaceLog::class)->weeklyOn(1, '14:00');
         // $schedule->command(UpateLocations::class)->quarterly();
         // $schedule->command(UpateCards::class)->weekly();
     }
